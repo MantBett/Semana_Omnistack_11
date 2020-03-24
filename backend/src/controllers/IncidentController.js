8e713f09373814506e405515dd2e1d8a002e5a2b
Ana Beatriz Mantovani Bett <abmbett@hotmail.com>
2020-03-24 12:11:21 -0300

    lesson_02
    backend complete

---
+++ backend/src/controllers/IncidentController.js
@@ -0,0 +1,60 @@
+const connection = require('../database/connection');
+
+module.exports = {
+  async index(request, response){
+    const { page = 1 } = request.query;
+
+    const [ count ] = await connection('incidents')
+      .count();
+
+    const incidents = await connection('incidents')
+      .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+      .limit(5)
+      .offset((page - 1) * 5) //pulando o primeiro, vai ser sempre multiplo de 5
+      .select([
+        'incidents.*', 
+        'ongs.email', 
+        'ongs.whatsapp', 
+        'ongs.city', 
+        'ongs.uf'
+      ]);
+
+    response.header('X-Total-Count', count['count(*)']);
+
+    return response.json(incidents);
+  },
+
+  async create(request, response){
+    const { title, description, value } = request.body;
+    const ong_id = request.headers.authorization;
+
+    const [id] = await connection('incidents').insert({
+      title,
+      description,
+      value,
+      ong_id,
+    });
+
+    return response.json({ id });
+  },
+
+  async delete(request, response){
+    const { id } = request.params;
+    const ong_id = request.headers.authorization;
+
+    const incident = await connection('incidents')
+      .where('id', id)
+      .select('ong_id')
+      .first();
+    
+    if(incident.ong_id != ong_id){
+      return response.status(401).json({ error: 'Operation not permitted.'}); // 401 -> Unauthorized (https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status)
+    }
+
+    await connection('incidents')
+      .where('id', id)
+      .delete();
+
+    return response.status(204).send(); // 204 -> No Content (https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status)
+  },
+};
